8009396ccee512e0064d6f4a633c7878628482f8
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-06-30 20:00:36 +0300

    continue portfolio

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -8,70 +8,102 @@ import {InformationSkills} from '../common/components/information/informationSki
 export const Contacts = () => {
     return (
         <div className={s.contactsBlock}>
-                <div className={sContainer.container}>
-                   <div><Title title={'Contact Me'}/></div>
+            <div className={`${sContainer.container} ${s.containerContacts}`}>
+                <Title title={'Contact me'}/>
+                <div className={s.contacts}>
+                    <InformationSkills titleInfo={'Leave me a message'}
+                                       textInfo={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'}/>
+
                     <form className={s.form}>
-                    </form>
-                </div>
-        </div>
 
-
-
-             /*<div className={s.contactsBlock}>
-            <div className={s.container}>
-                <div className={`${sContainer.container} ${s.containerContacts}`}>
-                    <Title title={'Contact Me'}/>
-                    <form className={s.form}>
-                        <div className={s.columnContainer}>
-                            <div className={s.columnInner}>
-                                <div className={s.contactsSection}>
-                                    <InformationSkills titleInfo={'Leave me a message'}
-                                                       textInfo={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'}/>
-
-                                    <div className={s.col}>
-                                        <div className={s.minForm}>
-                                            <div className={s.formGroup}>
-                                                <input className={s.formControl} type='text' name={'your-name'}
-                                                       size={'40'}
-                                                       placeholder='Your Name' aria-required='true'
-                                                       aria-invalid='false'
-                                                style={{width: '85%', marginRight: '10px'}}/>
-                                            </div>
-                                            <div className={s.formGroup}>
-                                                <input className={s.formControl} type='text'
-                                                       name={'your email address'}
-                                                       size={'40'} placeholder='Your Email Address' aria-required='true'
-                                                       aria-invalid='false'
-                                                       style={{width: '85%'}}/>
-                                            </div>
-                                        </div>
-                                        <div className={s.maxForm}>
-                                            <div className={s.formGroup}>
-                                                <input className={s.formControl} type='text' name={'subject'}
-                                                       size={'40'}
-                                                       placeholder='Subject' aria-required='true' aria-invalid='false'
-                                                       style={{width: '100%'}}/>
-                                            </div>
-                                        </div>
-                                        <div className={s.maxForm}>
-                                            <div className={s.formGroup}>
-                                               <textarea style={{resize: 'none', height: '125px', width: '100%'}} className={s.formControl} name='message' cols='40' rows='10' aria-required='true'
-                                               aria-invalid='false' placeholder='Your Message'/>
-                                            </div>
-                                        </div>
-                                        <div className={s.pushForm}>
-                                            <input className={s.bottom} type={'submit'} value={'Submit'}/>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
+                        <div className={s.formGroup}>
+                            <input className={s.formControl} type='text' name={'your-name'}
+                                   size={'40'}
+                                   placeholder='Your Name' aria-required='true'
+                                   aria-invalid='false'/>
                         </div>
+
+                        <div className={s.formGroup}>
+                            <input className={s.formControl} type='text'
+                                   name={'your email address'}
+                                   size={'40'} placeholder='Your Email Address' aria-required='true'
+                                   aria-invalid='false'/>
+                        </div>
+
+                        <div className={s.formGroup}>
+                                <input className={s.formControl} type='text' name={'subject'}
+                                       size={'40'}
+                                       placeholder='Subject' aria-required='true' aria-invalid='false'/>
+                        </div>
+
+                        <div className={s.formGroup}>
+                                          <textarea className={s.formControl} name='message' cols='40' rows='10' aria-required='true'
+                                                    aria-invalid='false' placeholder='Your Message'/>
+                        </div>
+                        <div className={s.pushForm}>
+                            <input className={s.bottom} type={'submit'} value={'Submit'}/>
+                        </div>
+
                     </form>
                 </div>
-
             </div>
         </div>
+
+
+
+        /*<div className={s.contactsBlock}>
+       <div className={s.container}>
+           <div className={`${sContainer.container} ${s.containerContacts}`}>
+               <Title title={'Contact Me'}/>
+               <form className={s.form}>
+                   <div className={s.columnContainer}>
+                       <div className={s.columnInner}>
+                           <div className={s.contactsSection}>
+
+                               <div className={s.col}>
+                                   <div className={s.minForm}>
+                                       <div className={s.formGroup}>
+                                           <input className={s.formControl} type='text' name={'your-name'}
+                                                  size={'40'}
+                                                  placeholder='Your Name' aria-required='true'
+                                                  aria-invalid='false'
+                                           style={{width: '85%', marginRight: '10px'}}/>
+                                       </div>
+                                       <div className={s.formGroup}>
+                                           <input className={s.formControl} type='text'
+                                                  name={'your email address'}
+                                                  size={'40'} placeholder='Your Email Address' aria-required='true'
+                                                  aria-invalid='false'
+                                                  style={{width: '85%'}}/>
+                                       </div>
+                                   </div>
+                                   <div className={s.maxForm}>
+                                       <div className={s.formGroup}>
+                                           <input className={s.formControl} type='text' name={'subject'}
+                                                  size={'40'}
+                                                  placeholder='Subject' aria-required='true' aria-invalid='false'
+                                                  style={{width: '100%'}}/>
+                                       </div>
+                                   </div>
+                                   <div className={s.maxForm}>
+                                       <div className={s.formGroup}>
+                                          <textarea style={{resize: 'none', height: '125px', width: '100%'}} className={s.formControl} name='message' cols='40' rows='10' aria-required='true'
+                                          aria-invalid='false' placeholder='Your Message'/>
+                                       </div>
+                                   </div>
+                                   <div className={s.pushForm}>
+                                       <input className={s.bottom} type={'submit'} value={'Submit'}/>
+                                   </div>
+                               </div>
+
+                           </div>
+                       </div>
+                   </div>
+               </form>
+           </div>
+
+       </div>
+   </div>
 */
     )
 }
--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,7 +2,6 @@ import s from './Footer.module.css';
 import sContainer from "../common/styles/Container.module.css";
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {NavLink} from "react-router-dom";
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
 import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
 import {faTelegram} from "@fortawesome/free-brands-svg-icons";
@@ -14,14 +13,14 @@ export function Footer() {
         <div className={s.footerBlock}>
             <div className={`${sContainer.container} ${s.footerContainer}`}>
                 <div><h3>Petr Zhirkov</h3></div>
-                <div className={s.main}>
+                <div>
                     <div className={s.contacts}>
-                        <NavLink to="/telegram" target='_blank'><FontAwesomeIcon className={s.icon} style={{color: '#ed1c24'}} icon={faTelegram}/></NavLink>
-                        <NavLink to="/linkedId" target='_blank'><FontAwesomeIcon className={s.icon} style={{color: '#e10337'}} icon={faLinkedin}/></NavLink>
-                        <NavLink to="/github" target='_blank'><FontAwesomeIcon className={s.icon} style={{color: '#b90162'}}
-                                                                               icon={faGithub}/></NavLink>
-                        <NavLink to="/instagram" target='_blank'><FontAwesomeIcon className={s.icon} style={{color: '#873e95'}}
-                                                                                  icon={faInstagram}/></NavLink>
+                        <a href='https://t.me/zhirkovpetr' target='_blank'><FontAwesomeIcon className={s.icon} style={{color: '#ed1c24'}} icon={faTelegram}/></a>
+                        <a href='https://www.linkedin.com/in/zhirkovpetr/' target='_blank'><FontAwesomeIcon className={s.icon} style={{color: '#e10337'}} icon={faLinkedin}/></a>
+                        <a href='https://github.com/zhirkovpetr' target='_blank'><FontAwesomeIcon className={s.icon} style={{color: '#b90162'}}
+                                                                               icon={faGithub}/></a>
+                        <a href='https://www.instagram.com/zhirkov_petr/' target='_blank'><FontAwesomeIcon className={s.icon} style={{color: '#873e95'}}
+                                                                                  icon={faInstagram}/></a>
                     </div>
                 </div>
                 <div className={s.reserved}>
