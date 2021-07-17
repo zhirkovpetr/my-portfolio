c29c812fdbf6584f81901eaa248b34ce1265882e
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-07-17 17:07:04 +0300

    refactoring

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,6 +4,7 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub, faInstagram, faLinkedin, faTelegram} from "@fortawesome/free-brands-svg-icons";
 import MyImage from '../assets/pictures/photo_2021-06-21_15-26-44.jpg';
+import {Nav} from './nav/Nav';
 
 
 export const Header = () => {
@@ -19,28 +20,10 @@ export const Header = () => {
                     <div className={s.images} style={myImages}/>
                     <div><h3>Petr Zhirkov</h3></div>
                 </div>
+
+                <Nav />
                 <div className={s.headerBurger} >
                     <span> </span>
-                </div>
-
-                <div className={s.nav}>
-                    <nav className={s.mainMenu}>
-                        <ul className={s.navbar}>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#f4bf00'}} href={''}>home</a></li>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#f7941d'}} href={''}>skills</a></li>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#f26522'}} href={''}>my works</a></li>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#ed1c24'}} href={''}>contacts</a></li>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#e10337'}} href={''}>resume</a></li>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#b90162'}} href={'https://t.me/zhirkovpetr'} target='_blank'><FontAwesomeIcon className={s.icon}
-                                                                                                                                           icon={faTelegram}/></a></li>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#873e95'}} href={'https://www.linkedin.com/in/zhirkovpetr/'} target='_blank'><FontAwesomeIcon className={s.icon}
-                                                                                                                                           icon={faLinkedin}/></a></li>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#3c70b4'}} href={'https://github.com/zhirkovpetr'} target='_blank'><FontAwesomeIcon className={s.icon}
-                                                                                                                                         icon={faGithub}/></a></li>
-                            <li className={s.mainMenuItem}><a style={{backgroundColor: '#13376a'}} href={'https://www.instagram.com/zhirkov_petr/'} target='_blank'><FontAwesomeIcon className={s.icon}
-                                                                                                                                            icon={faInstagram}/></a></li>
-                        </ul>
-                    </nav>
                 </div>
             </div>
         </div>
---
+++ src/header/nav/Nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import s from './Nav.module.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faInstagram, faLinkedin, faTelegram} from "@fortawesome/free-brands-svg-icons";
+
+
+export const Nav = () => {
+    return (
+        <div className={s.nav}>
+            <nav className={s.mainMenu}>
+                <ul className={s.navbar}>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#f4bf00'}} href={''}>home</a></li>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#f7941d'}} href={''}>skills</a></li>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#f26522'}} href={''}>my works</a></li>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#ed1c24'}} href={''}>contacts</a></li>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#e10337'}} href={''}>resume</a></li>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#b90162'}} href={'https://t.me/zhirkovpetr'} target='_blank'><FontAwesomeIcon className={s.icon}
+                                                                                                                                                              icon={faTelegram}/></a></li>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#873e95'}} href={'https://www.linkedin.com/in/zhirkovpetr/'} target='_blank'><FontAwesomeIcon className={s.icon}
+                                                                                                                                                                              icon={faLinkedin}/></a></li>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#3c70b4'}} href={'https://github.com/zhirkovpetr'} target='_blank'><FontAwesomeIcon className={s.icon}
+                                                                                                                                                                    icon={faGithub}/></a></li>
+                    <li className={s.mainMenuItem}><a style={{backgroundColor: '#13376a'}} href={'https://www.instagram.com/zhirkov_petr/'} target='_blank'><FontAwesomeIcon className={s.icon}
+                                                                                                                                                                             icon={faInstagram}/></a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
